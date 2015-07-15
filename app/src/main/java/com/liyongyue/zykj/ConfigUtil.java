b03ed021bba78429762b3f7236e6fa9543a0dd43
yli <liyongyuea@126.com>
2015-07-15 13:30:25 +0800

    添加了伪装loation的功能

--- app/src/main/java/com/liyongyue/zykj/ConfigUtil.java
+++ app/src/main/java/com/liyongyue/zykj/ConfigUtil.java
@@ -1,20 +1,103 @@
 package com.liyongyue.zykj;
 
+import android.util.Log;
+
+import org.w3c.dom.Element;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Random;
 
 /**
- * Created by yli on 2015/7/1.
+ * Created by Administrator on 2015/7/6.
  */
 public class ConfigUtil {
-    private static String fileName = "";
-    static{
-        File configFile = new File(fileName);
+    private static Properties properties = new Properties();
+    private static final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    private static final String hex = "0123456789abcdef";
+    private static final String upCase = lowerCase.toUpperCase();
+    private static final String num = "0123456789";
+    public static void init(){
+
+
+        String fileName = "/assets/config.properties";
+        try {
+            properties.load(ConfigUtil.class.getResourceAsStream(fileName));
+            Enumeration enu2=properties.propertyNames();
+            while(enu2.hasMoreElements()){
+                String key = (String)enu2.nextElement();
+                Log.e("input", "properties key:" + key + "result : " + properties.get(key));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String get(String key){
-        return null;
+
+        String result = (String)properties.get(key);
+        Log.e("input", "get key:" + key + "result:" + result);
+        return result;
+    }
+
+    public static String getMAC(){
+        String result = (String)properties.get("MAC");
+        if(result==null){
+
+        }else if(result == "random"){
+            String mac = getRandomString(hex,12).toUpperCase();
+            result = mac.substring(0,2) + ":";
+            result = mac.substring(2,4) + ":";
+            result = mac.substring(4,6) + ":";
+            result = mac.substring(6,8) + ":";
+            result = mac.substring(8,10) + ":";
+            result = mac.substring(10,12);
+        }
+        return result;
+    }
+
+    public static String getIMSI(){
+        String result = (String)properties.get("IMSI");
+        String preYD1 = "46000";
+        String preYD2 = "46002";
+        String preLT = "46001";
+        String preDX = "46003";
+        if(result==null){
+
+        }else if(result == "random"){
+            String mac = getRandomString(hex,12).toUpperCase();
+            result = mac.substring(0,2) + ":";
+            result = mac.substring(2,4) + ":";
+            result = mac.substring(4,6) + ":";
+            result = mac.substring(6,8) + ":";
+            result = mac.substring(8,10) + ":";
+            result = mac.substring(10,12);
+        }
+        return result;
     }
 
 
+    private static String getRandomString(int length) {
+        String base = "0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    public static String getRandomString(String base, int length){
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
 
 }
--- app/src/main/java/com/liyongyue/zykj/WZUtil.java
+++ app/src/main/java/com/liyongyue/zykj/WZUtil.java
@@ -1,5 +1,10 @@
 package com.liyongyue.zykj;
 
+import android.annotation.TargetApi;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.SystemClock;
 import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -13,13 +18,13 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class WZUtil implements IXposedHookLoadPackage{
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-
+        if ( loadPackageParam.packageName.equals("com.liyongyue.getinfo") ){
         try{
-
+            ConfigUtil.init();
             Class<?> classBuild = XposedHelpers.findClass("android.os.Build", loadPackageParam.classLoader);
-            String saveModel        = "Samsung Galaxy S6";
-            String saveManufacutrer = "Sumsung";
-            String saveBrand = "Galaxy";
+            String saveModel        = ConfigUtil.get("MODEL");
+            String saveManufacutrer = ConfigUtil.get("MANU");
+            String saveBrand = ConfigUtil.get("BRAND");
             if (!saveManufacutrer.equals("null") && !saveManufacutrer.equals("")) XposedHelpers.setStaticObjectField(classBuild, "MANUFACTURER", saveManufacutrer);
             if (!saveModel.equals("null") && !saveModel.equals("")) XposedHelpers.setStaticObjectField(classBuild, "MODEL", saveModel);
             if (!saveBrand.equals("null") && !saveBrand.equals("")) XposedHelpers.setStaticObjectField(classBuild, "BRAND", saveBrand);
@@ -29,27 +34,23 @@ public class WZUtil implements IXposedHookLoadPackage{
 //            if (!saveAndroidId.equals("null") && !saveAndroidId.equals("")) XposedHelpers.setStaticObjectField(classSetting, "ANDROID_ID", saveAndroidId);
 
             Class<?> classVERSION = XposedHelpers.findClass("android.os.Build.VERSION",loadPackageParam.classLoader);
-            String saveSDK = "19";
-            String saveVersion = "4.4.5";
+            String saveSDK = ConfigUtil.get("SDK");
+            String saveVersion = ConfigUtil.get("VERSION");
             if (!saveSDK.equals("null") && !saveSDK.equals("")) XposedHelpers.setStaticObjectField(classVERSION, "SDK", saveSDK);
-            if (!saveSDK.equals("null") && !saveSDK.equals("")) XposedHelpers.setStaticObjectField(classVERSION, "RELEASE", saveVersion);
-
-
+            if (!saveVersion.equals("null") && !saveVersion.equals("")) XposedHelpers.setStaticObjectField(classVERSION, "RELEASE", saveVersion);
 
             Class<?> classTelephonyManager = XposedHelpers.findClass("android.telephony.TelephonyManager", loadPackageParam.classLoader);
             Class<?> classWifiInfo         = XposedHelpers.findClass("android.net.wifi.WifiInfo", loadPackageParam.classLoader);
             Class<?> classSettingsSecure   = XposedHelpers.findClass("android.provider.Settings.Secure", loadPackageParam.classLoader);
-
+            Class<?> classLocationManager = XposedHelpers.findClass("android.location.LocationManager", loadPackageParam.classLoader);
             XposedHelpers.findAndHookMethod(classTelephonyManager, "getDeviceId", new XC_MethodHook()
             {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable
                 {
-                    String saveValue = "111111111111111";
-                    Log.e("IMEI",param.getResult().toString());
+                    String saveValue = ConfigUtil.get("IMEI");
                     if (!saveValue.equals("null") && !saveValue.equals("")) param.setResult(saveValue);
                     super.afterHookedMethod(param);
-                    Log.e("IMEI", param.getResult().toString());
                 }
             });
 
@@ -59,25 +60,76 @@ public class WZUtil implements IXposedHookLoadPackage{
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable
                 {
-                    String saveValue = "12:23:34:45:56:67";
+                    String saveValue = ConfigUtil.get("MAC");
                     if (!saveValue.equals("null") && !saveValue.equals("")) param.setResult(saveValue);
                     super.afterHookedMethod(param);
                 }
             });
 
-            XposedHelpers.findAndHookMethod(classSettingsSecure, "getString", android.content.ContentResolver.class , String.class, new XC_MethodHook()
+            XposedHelpers.findAndHookMethod(classLocationManager, "getLastKnownLocation" , String.class, new XC_MethodHook()
             {
+                @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable
                 {
-                    String saveValue = "1234567890abcdef";
-                    if (param.args[1].equals("android_id") && !saveValue.equals("null")  && !saveValue.equals("")) param.setResult(saveValue);
+                    if(param.getResult() != null)
+                        Log.e("input loc before",param.getResult().toString());
+                    else
+                        Log.e("input loc before","location is null");
+                    String saveValue = ConfigUtil.get("GPS");
+                    if (!saveValue.equals("null")  && !saveValue.equals("")){
+                        String[] gps = saveValue.split(",");
+                        double lat = Double.valueOf(gps[0]);
+                        double lon = Double.valueOf(gps[1]);
+                        Location location = new Location(LocationManager.GPS_PROVIDER);
+                        location.setLatitude(lat);
+                        location.setLongitude(lon);
+                        location.setAccuracy(70);
+                        location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+                        param.setResult(location);
+                    }
+                    Log.e("input loc after",param.getResult().toString());
                     super.afterHookedMethod(param);
                 }
             });
+
+            XposedHelpers.findAndHookMethod(classSettingsSecure, "getString", android.content.ContentResolver.class, String.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    String saveValue = ConfigUtil.get("ANDROIDID");
+                    if (param.args[1].equals("android_id") && !saveValue.equals("null") && !saveValue.equals(""))
+                        param.setResult(saveValue);
+                    Log.e("input after hook", param.getResult().toString());
+                }
+            });
+            Class<?> classLocationListener = XposedHelpers.findClass("android.location.LocationListener", loadPackageParam.classLoader);
+            XposedHelpers.findAndHookMethod(classLocationListener, "onLocationChanged", android.location.Location.class,new XC_MethodHook() {
+                @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    Log.e("input loc before",param.args[0].toString());
+                    String saveValue = ConfigUtil.get("GPS");
+                    if (!saveValue.equals("null")  && !saveValue.equals("")){
+                        String[] gps = saveValue.split(",");
+                        double lat = Double.valueOf(gps[0]);
+                        double lon = Double.valueOf(gps[1]);
+                        Location location = new Location(LocationManager.GPS_PROVIDER);
+                        location.setLatitude(lat);
+                        location.setLongitude(lon);
+                        location.setAccuracy(70);
+                        location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+                        param.args[0] = location;
+                    }
+                    Log.e("input loc after",param.args[0].toString());
+                }
+
+            });
+
+
         }catch (Exception e){
-            Log.e("IMEI",e.getStackTrace().toString());
+            Log.e("input","HOOK Error",e);
         }
 
+    }
     }
 }
